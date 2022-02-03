3d4b8e99b8486237ac848841a056b20aabf39c94
Jean-Sebastien Paquet <paquet@cadwork.ca>
2022-02-03 17:31:07 -0500

    Modifies build steps.

---
+++ src/menu_controller/__init__.py
@@ -0,0 +1,4 @@
+from typing import List
+
+
+def display_simple_menu(items: List[str]) -> str: ...
--- src/menu_controller/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from typing import List
-
-
-def display_simple_menu(items: List[str]) -> str: ...
