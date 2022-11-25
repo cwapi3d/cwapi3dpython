99f4ef33afe98ad848e2a4ca6269a501bf5c6f67
Michael Brunner <brunner@cadwork.swiss>
2022-11-25 08:01:06 +0100

    update package.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='cwapi3d',
-    version='1.3.12',
+    version='1.3.13',
     author='Cadwork',
     author_email='it@cadwork.ca',
     description='Python bindings for CwAPI3D',
