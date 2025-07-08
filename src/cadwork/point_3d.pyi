dad2f40b589bed6bd8c2914e6202159c64030c73
Michael Brunner <brunner@cadwork.swiss>
2025-07-08 11:03:36 +0200

    Add examples to function docstrings in various modules
    Signed-off-by: Michael Brunner <brunner@cadwork.swiss>

--- src/cadwork/point_3d.pyi
+++ src/cadwork/point_3d.pyi
@@ -8,6 +8,9 @@ class point_3d:
             y (float): The y-coordinate of the point.
             z (float): The z-coordinate of the point.
         """
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __add__(self, other: 'point_3d') -> 'point_3d':
         """
@@ -148,99 +151,3 @@ class point_3d:
 
     def invert(self) -> 'point_3d':
         """Return the inverted point (negated coordinates)."""
-    def __sub__(self, other: 'point_3d') -> 'point_3d':
-        """sub
-
-        Parameters:
-            other: other
-
-        Returns:
-            'point_3d'
-        """
-
-    def __truediv__(self, other: float) -> 'point_3d':
-        """truediv
-
-        Parameters:
-            other: other
-
-        Returns:
-            'point_3d'
-        """
-
-    def __iadd__(self, other: 'point_3d') -> 'point_3d':
-        """iadd
-
-        Parameters:
-            other: other
-
-        Returns:
-            'point_3d'
-        """
-
-    def __isub__(self, other: 'point_3d') -> 'point_3d':
-        """isub
-
-        Parameters:
-            other: other
-
-        Returns:
-            'point_3d'
-        """
-
-    def __imul__(self, other: float) -> 'point_3d':
-        """imul
-
-        Parameters:
-            other: other
-
-        Returns:
-            'point_3d'
-        """
-
-    def __itruediv__(self, other: float) -> 'point_3d':
-        """itruediv
-
-        Parameters:
-            other: other
-
-        Returns:
-            'point_3d'
-        """
-
-    def __neg__(self) -> 'point_3d':
-        """neg
-
-        Returns:
-            'point_3d'
-        """
-
-    def __eq__(self, other: object) -> bool:
-        """eq
-
-        Parameters:
-            other: other
-
-        Returns:
-            bool
-        """
-
-    def __ne__(self, other: object) -> bool:
-        """neg
-
-        Parameters:
-            other: other
-
-        Returns:
-            bool
-        """
-
-    def __add__(self, other: 'point_3d') -> 'point_3d':
-        """add
-
-        Parameters:
-            other: other
-
-        Returns:
-            'point_3d'
-        """
