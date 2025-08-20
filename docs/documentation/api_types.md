aa9e96fb969be5729da5bbd7028ac2f25c368685
Laura Bobillier <bobillier@caddev.fr>
2025-08-20 10:17:15 +0200

    [API-519] Added a section about custom types

---
+++ docs/documentation/api_types.md
@@ -0,0 +1,6 @@
+# Api Types
+
+::: cadwork.api_types
+    rendering:
+        show_root_heading: false
+        show_source: true
---
+++ src/cadwork/api_types.pyi
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+UnsignedInt = Annotated[int, "Must be >= 0"]
+""""""
+MaterialId = Annotated[int, "Must be >= 0"]
+""""""
+ColorId = Annotated[int, "Must be >= 0"]
+""""""
+EndtypeId = Annotated[int, "Must be >= 0"]
+""""""
+AxisId = Annotated[int, "Must be >= 0"]
+""""""
+MenuIndex = Annotated[int, "Normal int"]
+""""""
+ReferenceSide = Annotated[int, "Must be >= 0"]
+""""""
+MultiLayerSetId = Annotated[int, "Must be >= 0"]
+""""""
+UserAttributeId = Annotated[int, "Must be >= 0"]
+""""""
+ElementId = Annotated[int, "Must be >= 0"]
+""""""
