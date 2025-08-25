e70827b34c95b78b7d9fcc90b8c7e3a029bea446
Laura Bobillier <bobillier@caddev.fr>
2025-08-25 08:46:09 +0200

    [API-519] Improved the shop drawing controller documentation

--- src/shop_drawing_controller/__init__.pyi
+++ src/shop_drawing_controller/__init__.pyi
@@ -1,194 +1,140 @@
 from typing import List
 from cadwork.point_3d import point_3d
+from cadwork.api_types import *
 
 
-def export_2d_wireframe_with_clipboard(clipboard_number: int, with_layout: bool) -> None:
-    """Exports a 2D wireframe to the clipboard
+def export_2d_wireframe_with_clipboard(clipboard_number: UnsignedInt, with_layout: bool) -> None:
+    """Exports a 2D wireframe to the clipboard.
 
     Parameters:
-        clipboard_number: clipboard_number
-        with_layout: with_layout
-
-    Returns:
-        None
+        clipboard_number: The clipboard number.
+        with_layout: Use layout, false by default.
     """
 
-def export_2d_hidden_lines_with_clipboard(clipboard_number: int, with_layout: bool) -> None:
-    """Exports a 2D hidden lines to the clipboard
+def export_2d_hidden_lines_with_clipboard(clipboard_number: UnsignedInt, with_layout: bool) -> None:
+    """Exports a 2D hidden lines to the clipboard.
 
     Parameters:
-        clipboard_number: clipboard_number
-        with_layout: with_layout
-
-    Returns:
-        None
+        clipboard_number: The clipboard number.
+        with_layout: Use layout, false by default.
     """
 
 def export_2d_wireframe_with_2dc(file_path: str, with_layout: bool) -> None:
-    """Exports a 2D wireframe to a 2DC file
+    """Exports a 2D wireframe to a 2DC file.
 
     Parameters:
-        file_path: file_path
-        with_layout: with_layout
-
-    Returns:
-        None
+        file_path: The export file path.
+        with_layout: Use layout, false by default.
     """
 
 def export_2d_hidden_lines_with_2dc(file_path: str, with_layout: bool) -> None:
-    """Exports a 2D hidden lines to a 2DC file
+    """Exports a 2D hidden lines to a 2DC file.
 
     Parameters:
-        file_path: file_path
-        with_layout: with_layout
-
-    Returns:
-        None
+        file_path: The export file path.
+        with_layout: Use layout, false by default.
     """
 
-def export_wall_with_clipboard(clipboard_number: int, element_id_list: List[int]) -> None:
-    """Exports a wall to the clipboard
+def export_wall_with_clipboard(clipboard_number: UnsignedInt, element_id_list: List[ElementId]) -> None:
+    """Exports a wall to the clipboard.
 
     Parameters:
-        clipboard_number: clipboard_number
-        element_id_list: element_id_list
-
-    Returns:
-        None
+        clipboard_number: The clipboard number.
+        element_id_list: The elements to export.
     """
 
-def export_export_solid_with_clipboard(clipboard_number: int, element_id_list: List[int]) -> None:
-    """Exports an export solid to the clipboard
+def export_export_solid_with_clipboard(clipboard_number: UnsignedInt, element_id_list: List[ElementId]) -> None:
+    """Exports an export solid to the clipboard.
 
     Parameters:
-        clipboard_number: clipboard_number
-        element_id_list: element_id_list
-
-    Returns:
-        None
+        clipboard_number: The clipboard number.
+        element_id_list: The elements to export.
     """
 
-def export_piece_by_piece_with_clipboard(clipboard_number: int, element_id_list: List[int]) -> None:
-    """Exports a piece-by-piece to the clipboard
+def export_piece_by_piece_with_clipboard(clipboard_number: UnsignedInt, element_id_list: List[ElementId]) -> None:
+    """Exports a piece-by-piece to the clipboard.
 
     Parameters:
-        clipboard_number: clipboard_number
-        element_id_list: element_id_list
-
-    Returns:
-        None
+        clipboard_number: The clipboard number.
+        element_id_list: The elements to export.
     """
 
-def assign_export_solid(ceo_element: List[int], element_id_list: List[int]) -> None:
-    """Assigns elements to an export solid
+def assign_export_solid(ceo_element_id_list: List[ElementId], element_id_list: List[ElementId]) -> None:
+    """Assigns elements to an export solid.
 
     Parameters:
-        ceo_element: ceo_element
-        element_id_list: element_id_list
-
-    Returns:
-        None
+        ceo_element_id_list: The export solid to assign.
+        element_id_list: The elements to assign.
     """
 
-def export_container_with_clipboard(clipboard_number: int, elements: List[int]) -> None:
-    """Export a container to the clipboard
+def export_container_with_clipboard(clipboard_number: UnsignedInt, element_id_list: List[ElementId]) -> None:
+    """Export a container to the clipboard.
 
     Parameters:
-        clipboard_number: clipboard_number
-        elements: elements
-
-    Returns:
-        None
+        clipboard_number: The clipboard number.
+        element_id_list: The elements to export.
     """
 
-def add_wall_section_horizontal(element: int, position: point_3d) -> None:
-    """Adds a horizontal wall section
+def add_wall_section_horizontal(element_id: ElementId, position: point_3d) -> None:
+    """Adds a horizontal wall section.
 
     Parameters:
-        element: element
-        position: position
-
-    Returns:
-        None
+        element_id: The element id.
+        position: The section position.
     """
 
-def add_wall_section_vertical(element: int, position: point_3d) -> None:
-    """Adds a vertical wall section
+def add_wall_section_vertical(element_id: ElementId, position: point_3d) -> None:
+    """Adds a vertical wall section.
 
     Parameters:
-        element: element
-        position: position
-
-    Returns:
-        None
+        element_id: The element id.
+        position: The section position.
     """
 
-def export_wall_with_clipboard_and_presetting(clipboard_number: int, element_id_list: List[int], presetting_file: str) -> None:
-    """Exports a wall to the clipboard
+def export_wall_with_clipboard_and_presetting(clipboard_number: UnsignedInt, element_id_list: List[ElementId], presetting_file: str) -> None:
+    """Exports a wall to the clipboard.
 
     Parameters:
-        clipboard_number: clipboard_number
-        element_id_list: element_id_list
-        presetting_file: presetting_file
-
-    Returns:
-        None
+        clipboard_number: The clipboard number.
+        element_id_list: The element id list to export.
+        presetting_file: The presetting file path.
     """
 
 def load_export_piece_by_piece_settings(settings_file_path: str) -> None:
-    """Loads piece by piece export settings
+    """Loads piece by piece export settings.
 
     Parameters:
-        settings_file_path: settings_file_path
-
-    Returns:
-        None
+        settings_file_path: The settings file path.
     """
 
 def save_export_piece_by_piece_settings(settings_file_path: str) -> None:
-    """Saves piece by piece export settings
+    """Saves piece by piece export settings.
 
     Parameters:
-        settings_file_path: settings_file_path
-
-    Returns:
-        None
+        settings_file_path: The settings file path.
     """
 
 def clear_errors() -> None:
-    """clear errors
-
-    Returns:
-        None
+    """Clears all errors.
     """
 
 def load_export_wall_settings(settings_file_path: str) -> None:
-    """Loads wall export settings
+    """Loads wall export settings.
 
     Parameters:
-        settings_file_path: settings_file_path
-
-    Returns:
-        None
+        settings_file_path: The settings file path.
     """
 
 def load_export_solid_settings(settings_file_path: str) -> None:
-    """Loads export solid settings
+    """Loads export solid settings.
 
     Parameters:
-        settings_file_path: settings_file_path
-
-    Returns:
-        None
+        settings_file_path: The settings file path.
     """
 
 def load_export_container_settings(settings_file_path: str) -> None:
-    """Loads container export settings
+    """Loads container export settings.
 
     Parameters:
-        settings_file_path: settings_file_path
-
-    Returns:
-        None
+        settings_file_path: The settings file path.
     """
-
